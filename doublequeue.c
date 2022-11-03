1eb50b19e9ffde3f7bc23b22b0bdcffac7f8f06b
GOVIND V NAMBOOTHIRI <97877360+GVN101@users.noreply.github.com>
2022-11-03 15:52:23 +0530

    Add files via upload

---
+++ doublequeue.c
@@ -0,0 +1,148 @@
+#include<stdio.h>
+#include<math.h>
+#define size 50
+int f=-1,r=-1,ele,ch,t=0;
+int q[size];
+void insertf(int ele) 
+     {
+       if(f==(r+1)%size)
+         printf("Queue is full!!");
+       else
+        {
+         if(f==-1)
+           f=r=0;
+         else
+           {
+            if(f==0)
+              f=size-1;
+            else
+               f--;
+           }
+         q[f]=ele;
+         t++; 
+         printf("%d\t%d",f,t);
+        }  
+     }
+int deletef() 
+     {
+       if(f==-1)
+        {
+         printf("Queue is empty!!\n");
+         return -20;
+        }
+       else
+        {
+         ele=q[f];
+         t--;
+         if(f==r)
+           f=r=-1;
+         else
+           {
+            f=(f+1)%size;
+            return (ele);
+           }    
+        }  
+       return 0; 
+     }
+void insertr(int ele) 
+     {
+       if(f==(r+1)%size)
+         printf("Queue is full!!");
+       else
+        {
+         if(f==-1)
+           f=0;
+         r=(r+1)%size;
+         q[r]=ele;
+         t++;
+        }   
+     }
+int deleter() 
+     {
+       t--;   
+       if(f==r==-1)
+        {
+         printf("Queue is empty!!\n");
+         return -20;
+        }
+       else
+        {
+         ele=q[r];
+         if(f==r)
+           f=r=-1;
+         else
+           {
+            if(r==0)
+              r=size-1;
+            else
+             {
+              r--; 
+              return (ele);
+             } 
+           }    
+        }  
+       return 0; 
+     }     
+int display1()
+   { int c=1;
+      for(int j=f;c<=t;j=(j+1)%size,c++)
+         printf("%d\n",q[j]);
+ 
+      return 0;
+   }       
+void main()
+   {
+    do
+     {
+      printf("\n1.Insert front\n2.Delete front\n3.Insert rear\n4.Delete rear\n5.display\n6.exit\n");
+      printf("Enter your choice:");
+      scanf("%d",&ch);
+      switch(ch)
+        {
+         case 1: printf("Enter the element to be pushed:");
+                 scanf("%d",&ele);
+                 insertf(ele);
+                 break;  
+         case 2:
+                   ele=deletef();
+                   if(ele!=-20)
+                   printf("The element is:%d\n",ele);
+                 break;
+         case 3: printf("Enter the element to be pushed:");
+                 scanf("%d",&ele);
+                 insertr(ele);
+                 break;
+         case 4: ele=deleter();
+                  printf("The element is:%d\n",ele);
+                 break;
+         case 5: display1();
+                 break;
+         case 6: printf("Program terminated.\n");
+                 break;
+         default: printf("\nInvalid choice!!!\n");
+        }
+      }while(ch!=7);
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                     
